82aa3cb74f565c66e2871b0537c7b7c70e3cdc2c
Sunitha Devi <50926730+SunithaDeviP@users.noreply.github.com>
2020-03-23 10:35:03 +0530

    Add files via upload

---
+++ client-server-app/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"./server/api/user"
+)
+
+func main() {
+	// register static files handle '/index.html -> client/index.html'
+	http.Handle("/", http.FileServer(http.Dir("client")))
+	// register RESTful endpoint handler for '/users/'
+	http.Handle("/users/", &user.UserAPI{})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
